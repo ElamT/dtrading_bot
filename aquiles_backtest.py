69742b7edfbed6bf7fc9cf982e8091431a62c063
Elam Torres <torres.elam@gmail.com>
2023-11-08 10:22:08 -0500

    Save on file all trades made when the backtesting is runned

--- aquiles_backtest.py
+++ aquiles_backtest.py
@@ -10,47 +10,79 @@ class AquilesBacktest(AquilesBot):
     self.amount_lose = 0
     self.total_shares = 0
 
-  def run_backtesting(self):
+
+  def run_backtesting(self, year):
     index = 195
+    self.file_results = file = open("./results/%s_%s.csv" %(self.stock.symbol, year), "w")
+    self.file_columns = [
+      'date',
+      'order_type',
+      'volume',
+      'price',
+      'shares',
+      'stop_lose',
+      'take_profit',
+      'net',
+      'net_acum'
+    ]
+    header = "%s%s" %(','.join(self.file_columns), '\n')
+    self.file_results.write(header)
     for bar in self.bars[index:]:
       self.set_currents(index=index)
       self.process_bar()
       index += 1
 
+  def write_operation(self):
+    line = ""
+    for key in self.file_columns:
+      line += "%s," %(str(self.active_order[key]))
+
+    line+= "\n"
+    # breakpoint()
+    self.file_results.write(line)
+
   def analyze_active_order(self):
       if self.active_order['order_type'] == 'SELL':
           if self.current_bar.high >= self.active_order['stop_lose']:
-              self.amount_lose += self.active_order['shares'] * self.active_order['ticks_to_lose']
+              net = round(self.active_order['shares'] * self.active_order['ticks_to_lose'], 2)
+              self.amount_lose += net
               self.loses += 1
               self.total_shares += self.active_order['shares'] * 2
               self.active_order['status'] = 'LOSE'
-              self.active_order['NET'] = self.amount_win - self.amount_lose
-              # print(self.active_order)
+              self.active_order['net'] = net * -1
+              self.active_order['net_acum'] = round(self.amount_win - self.amount_lose, 2)
+              self.write_operation()
               self.active_order = None
 
           elif self.current_bar.low <= self.active_order['take_profit']:
-              self.amount_win += self.active_order['shares'] * self.active_order['ticks_to_win']
+              net = round(self.active_order['shares'] * self.active_order['ticks_to_win'], 2)
+              self.amount_win += net
               self.wins += 1
               self.total_shares += self.active_order['shares'] * 2
               self.active_order['status'] = 'WIN'
-              self.active_order['NET'] = self.amount_win - self.amount_lose
-              # print(self.active_order)
+              self.active_order['net'] = net
+              self.active_order['net_acum'] = round(self.amount_win - self.amount_lose, 2)
+              self.write_operation()
               self.active_order = None
       else:
           if self.current_bar.low <= self.active_order['stop_lose']:
-              self.amount_lose += self.active_order['shares'] * self.active_order['ticks_to_lose']
+              net = round(self.active_order['shares'] * self.active_order['ticks_to_lose'], 2)
+              self.amount_lose += net
               self.loses += 1
               self.total_shares += self.active_order['shares'] * 2
               self.active_order['status'] = 'LOSE'
-              self.active_order['NET'] = self.amount_win - self.amount_lose
-              # print(self.active_order)
+              self.active_order['net'] = net * -1
+              self.active_order['net_acum'] = round(self.amount_win - self.amount_lose, 2)
+              self.write_operation()
               self.active_order = None
 
           elif self.current_bar.high >= self.active_order['take_profit']:
-              self.amount_win += self.active_order['shares'] * self.active_order['ticks_to_win']
+              net = round(self.active_order['shares'] * self.active_order['ticks_to_win'], 2)
+              self.amount_win += net
               self.wins += 1
               self.total_shares += self.active_order['shares'] * 2
               self.active_order['status'] = 'WIN'
-              self.active_order['NET'] = self.amount_win - self.amount_lose
-              # print(self.active_order)
+              self.active_order['net'] = net
+              self.active_order['net_acum'] = round(self.amount_win - self.amount_lose, 2)
+              self.write_operation()
               self.active_order = None
--- run_backtesting.py
+++ run_backtesting.py
@@ -2,24 +2,14 @@ from aquiles_backtest import AquilesBacktest
 from datetime import date, datetime
 from ib_insync import BarData
 import os
-
+import glob
 
 for x in [2017, 2018, 2019, 2020, 2021, 2022, 2023]:
-    a = date(x, 1, 1)
-    b = date(x, 12, 31)
     aquiles = AquilesBacktest('AMD')
     aquiles.bars = []
+    target = sorted(glob.glob('./data/AMD_%s*' %(x)))
 
-    files = sorted(os.listdir('./data'))
-    target = []
-    for x in files:
-        file_date = datetime.strptime(x.split('_')[-1], '%Y%m%d').date()
-        if a <= file_date <= b:
-            target.append(x)
-
-    for file in target:
-        filename = "./data/%s" %(file)
-        # print('Processing %s' %(filename))
+    for filename in target:
         file = open(filename, "r")
         lines = file.readlines()
 
@@ -39,8 +29,8 @@ for x in [2017, 2018, 2019, 2020, 2021, 2022, 2023]:
 
         file.close
 
-    aquiles.run_backtesting()
-    print("======= %s - %s ========" %(a, b))
+    aquiles.run_backtesting(x)
+    print("======= %s ========" %(x))
     print("Loses: " + str(aquiles.amount_lose))
     print("Profits: " + str(aquiles.amount_win))
     print('NET: '+ str(aquiles.amount_win - aquiles.amount_lose))
@@ -48,4 +38,7 @@ for x in [2017, 2018, 2019, 2020, 2021, 2022, 2023]:
     print("Profits Count: " + str(aquiles.wins))
     print("TOTAL TRADES: %s" %(aquiles.loses + aquiles.wins))
     print('TOTAL SHARES: %s' %(aquiles.total_shares))
-    print("Effective: %s" %(round(aquiles.wins / (aquiles.wins + aquiles.loses) * 100, 2)))
+
+    total_bars = aquiles.wins + aquiles.loses
+    if total_bars > 0:
+      print("Effective: %s" %(round(aquiles.wins / (total_bars) * 100, 2)))
